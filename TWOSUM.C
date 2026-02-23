d3dc995a7ecd6414e5d030e5d826720e2315f557
MOHIT PANDIT <reachtomohit01@gmail.com>
2026-02-23 21:48:48 +0530

    Twosum.C

--- TWOSUM.C
+++ TWOSUM.C
@@ -1,3 +1,5 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,4 +21,5 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     
     *returnSize = 0;  // If no solution found
     return NULL;
+
 }
